4653f01c1b50c9370c197de5e4d7d18798e04994
yosadade <masyosad@gmail.com>
2020-05-20 21:00:32 +0700

    redux => reducer -> ourBurger

--- src/redux/reducer/ourBurgers/index.js
+++ src/redux/reducer/ourBurgers/index.js
@@ -1,4 +1,42 @@
 const INITIAL_STATE = {
+  meals: [
+    {
+      iconUri: 'https://live.staticflickr.com/1599/24209187444_bf2ed3a50a_w.jpg',
+      name: 'Hamburger',
+      price: 200,
+      isNewMenu: false
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/4160/34691586465_0c02d57aa4_w.jpg',
+      name: 'Cheese Burger',
+      price: 200,
+      isNewMenu: false
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/5347/9675671508_eab049e690_z.jpg',
+      name: 'Turkey Burger',
+      price: 200,
+      isNewMenu: false
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/437/18646499176_a830598129_w.jpg',
+      name: 'Malmo Burger',
+      price: 200,
+      isNewMenu: false
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/6165/6184774158_51c0daca53_b.jpg',
+      name: 'French Fries',
+      price: 200,
+      isNewMenu: false
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/4574/38182807165_52805fb3f4_w.jpg',
+      name: 'Onion Ring',
+      price: 200,
+      isNewMenu: false
+    }
+  ],
   salads: [
     {
       iconUri: 'https://live.staticflickr.com/1859/44622903591_4da9457b19_z.jpg',
@@ -55,6 +93,18 @@ const INITIAL_STATE = {
       name: 'Dessert Chocolate',
       price: 180,
       isNewMenu: true
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/2306/2058324119_6c3ec3e37e_w.jpg',
+      name: 'Lemon Squares and Key Lime Bars',
+      price: 180,
+      isNewMenu: true
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/5136/5525093443_1471187a3b_w.jpg',
+      name: 'Pattiserie',
+      price: 180,
+      isNewMenu: true
     }
   ],
   baverages: [
@@ -79,6 +129,12 @@ const INITIAL_STATE = {
     {
       iconUri: 'https://live.staticflickr.com/7713/17228322916_de4c224658_w.jpg',
       name: 'Teh Botol Sosro',
+      price: 120,
+      isNewMenu: true
+    },
+    {
+      iconUri: 'https://live.staticflickr.com/2770/32053276334_f76d3a73c1_w.jpg',
+      name: 'Fanta',
       price: 120,
       isNewMenu: true
     }
